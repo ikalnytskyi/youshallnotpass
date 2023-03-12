ef9941408a4a959999adb049a79e77429ed93f6c
Ihor Kalnytskyi <ihor@kalnytskyi.com>
2023-03-12 16:16:09 +0200

    Make TokenBucket Sync
    We are using a Mutex internally right now, but we should try to
    refactor this to use atomic operations instead. That will require
    finding an alternative to Instant, though, as it is opaque, and
    does not allow us to access the internal u64 counter, which we could
    increment atomically.

--- src/token_bucket.rs
+++ src/token_bucket.rs
@@ -1,41 +1,47 @@
+use std::sync::Mutex;
 use std::time::{Duration, Instant};
 
 pub struct TokenBucket {
     time_per_token: usize,
     interval: Duration,
-    last_replenished_at: Option<Instant>,
-    clock: Box<dyn Fn() -> Instant>,
+    last_replenished_at: Mutex<Option<Instant>>,
+    clock: Box<dyn Fn() -> Instant + Sync>,
 }
 
 impl TokenBucket {
     pub fn new(limit: usize, interval: Duration) -> Self {
+        TokenBucket::with_timer(limit, interval, Box::new(Instant::now))
+    }
+
+    pub fn with_timer(
+        limit: usize,
+        interval: Duration,
+        clock: Box<dyn Fn() -> Instant + Sync>,
+    ) -> Self {
         assert!(limit > 0);
 
-        Self {
+        TokenBucket {
             time_per_token: interval.as_nanos() as usize / limit,
             interval,
-            last_replenished_at: None,
-            clock: Box::new(Instant::now),
+            last_replenished_at: Mutex::new(None),
+            clock,
         }
     }
 
-    pub fn with_timer(mut self, clock: Box<dyn Fn() -> Instant>) -> Self {
-        self.clock = clock;
-        self
-    }
-
-    pub fn consume(&mut self, tokens: usize) -> bool {
+    pub fn consume(&self, tokens: usize) -> bool {
         let now = (self.clock)();
+
+        let mut lock = self.last_replenished_at.lock().unwrap();
 
         let interval_start = now.checked_sub(self.interval).unwrap_or(now);
         let token_delay = Duration::from_nanos((tokens * self.time_per_token) as u64);
-        let last_replenished_at = self.last_replenished_at.unwrap_or(interval_start);
+        let last_replenished_at = lock.unwrap_or(interval_start);
 
         let required_time = std::cmp::max(interval_start, last_replenished_at) + token_delay;
         if required_time > now {
             false
         } else {
-            self.last_replenished_at = Some(required_time);
+            *lock = Some(required_time);
             true
         }
     }
@@ -45,39 +51,44 @@ impl TokenBucket {
 mod tests {
     use super::*;
 
-    use std::cell::RefCell;
-    use std::rc::Rc;
+    use std::sync::{Arc, Mutex};
 
     #[test]
     fn capacity_is_one() {
-        let now = Rc::new(RefCell::new(Instant::now()));
+        let now = Arc::new(Mutex::new(Instant::now()));
         let now_moved = now.clone();
 
-        let mut bucket = TokenBucket::new(1, Duration::from_secs(1))
-            .with_timer(Box::new(move || *now_moved.borrow()));
+        let bucket = TokenBucket::with_timer(
+            1,
+            Duration::from_secs(1),
+            Box::new(move || *now_moved.lock().unwrap()),
+        );
 
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), false);
 
-        *now.borrow_mut() += Duration::from_secs(1);
+        *now.lock().unwrap() += Duration::from_secs(1);
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), false);
     }
 
     #[test]
     fn capacity_gt_one() {
-        let now = Rc::new(RefCell::new(Instant::now()));
+        let now = Arc::new(Mutex::new(Instant::now()));
         let now_moved = now.clone();
 
-        let mut bucket = TokenBucket::new(3, Duration::from_secs(1))
-            .with_timer(Box::new(move || *now_moved.borrow()));
+        let bucket = TokenBucket::with_timer(
+            3,
+            Duration::from_secs(1),
+            Box::new(move || *now_moved.lock().unwrap()),
+        );
 
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), false);
 
-        *now.borrow_mut() += Duration::from_secs(1);
+        *now.lock().unwrap() += Duration::from_secs(1);
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), true);
@@ -86,19 +97,22 @@ mod tests {
 
     #[test]
     fn period_gt_one() {
-        let now = Rc::new(RefCell::new(Instant::now()));
+        let now = Arc::new(Mutex::new(Instant::now()));
         let now_moved = now.clone();
 
-        let mut bucket = TokenBucket::new(1, Duration::from_secs(3))
-            .with_timer(Box::new(move || *now_moved.borrow()));
+        let bucket = TokenBucket::with_timer(
+            1,
+            Duration::from_secs(3),
+            Box::new(move || *now_moved.lock().unwrap()),
+        );
 
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), false);
 
-        *now.borrow_mut() += Duration::from_secs(2);
+        *now.lock().unwrap() += Duration::from_secs(2);
         assert_eq!(bucket.consume(1), false);
 
-        *now.borrow_mut() += Duration::from_secs(3);
+        *now.lock().unwrap() += Duration::from_secs(3);
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), false);
     }
@@ -106,22 +120,25 @@ mod tests {
     #[test]
     fn consume_over_time() {
         let t0 = Instant::now();
-        let now = Rc::new(RefCell::new(t0.clone()));
+        let now = Arc::new(Mutex::new(t0.clone()));
         let now_moved = now.clone();
 
-        let mut bucket = TokenBucket::new(4, Duration::from_secs(1))
-            .with_timer(Box::new(move || *now_moved.borrow()));
+        let bucket = TokenBucket::with_timer(
+            4,
+            Duration::from_secs(1),
+            Box::new(move || *now_moved.lock().unwrap()),
+        );
 
         // consume first token
-        *now.borrow_mut() = t0;
+        *now.lock().unwrap() = t0;
         assert_eq!(bucket.consume(1), true);
 
         // consume second token
-        *now.borrow_mut() = t0 + Duration::from_millis(50);
+        *now.lock().unwrap() = t0 + Duration::from_millis(50);
         assert_eq!(bucket.consume(1), true);
 
         // consume third & fourth tokens
-        *now.borrow_mut() = t0 + Duration::from_millis(150);
+        *now.lock().unwrap() = t0 + Duration::from_millis(150);
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), true);
 
@@ -129,18 +146,18 @@ mod tests {
         assert_eq!(bucket.consume(1), false);
 
         // one token is not yet replenished
-        *now.borrow_mut() = t0 + Duration::from_millis(249);
+        *now.lock().unwrap() = t0 + Duration::from_millis(249);
         assert_eq!(bucket.consume(1), false);
 
         // one token is replenished
-        *now.borrow_mut() = t0 + Duration::from_millis(250);
+        *now.lock().unwrap() = t0 + Duration::from_millis(250);
         assert_eq!(bucket.consume(1), true);
 
         // ensure we are out of tokens again
         assert_eq!(bucket.consume(1), false);
 
         // two tokens are replenished
-        *now.borrow_mut() = t0 + Duration::from_millis(750);
+        *now.lock().unwrap() = t0 + Duration::from_millis(750);
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), false);
@@ -148,25 +165,28 @@ mod tests {
 
     #[test]
     fn weight_gt_one() {
-        let now = Rc::new(RefCell::new(Instant::now()));
+        let now = Arc::new(Mutex::new(Instant::now()));
         let now_moved = now.clone();
 
-        let mut bucket = TokenBucket::new(3, Duration::from_secs(1))
-            .with_timer(Box::new(move || *now_moved.borrow()));
+        let bucket = TokenBucket::with_timer(
+            3,
+            Duration::from_secs(1),
+            Box::new(move || *now_moved.lock().unwrap()),
+        );
 
         // consume all tokens at once
         assert_eq!(bucket.consume(3), true);
         assert_eq!(bucket.consume(1), false);
 
         // sequentially consume tokens
-        *now.borrow_mut() += Duration::from_secs(1);
+        *now.lock().unwrap() += Duration::from_secs(1);
         assert_eq!(bucket.consume(2), true);
         assert_eq!(bucket.consume(2), false);
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), false);
 
         // two tokens are replenished
-        *now.borrow_mut() += Duration::from_millis(700);
+        *now.lock().unwrap() += Duration::from_millis(700);
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), true);
         assert_eq!(bucket.consume(1), false);
